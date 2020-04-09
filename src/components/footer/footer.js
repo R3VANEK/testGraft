2d8ae558287d871885b6b6250739acc6bbbf6433
Arcywojak <maciej778@op.pl>
2020-04-09 10:18:02 +0200

    change footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,47 @@
 import React from 'react'
 import './footer.css'
+import fb from '../../images/facebook.svg';
 
 function Footer() {
     return (
         <footer>
-        <div class="footer">
-            <a>CoZa500Plus.pl</a>
-            <div class="footer-right">
-                <a href="#home">O Nas</a>
-                <a href="#contact">Kontakt</a>
-                <a href="#about">Logowanie</a>
+        <div class="footer-inner-1st">
+            <div class="footer-inner-1st-column">
+                <h3>Section 1</h3>
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 2</a>
+                <div class="footer-icons">
+                    <img src={fb} alt="fb" />
+                    <img src={fb} alt="fb" />
+                    <img src={fb} alt="fb" />
+                </div>               
+            </div>
+            <div class="footer-inner-1st-column">
+                <h3>Section 2</h3>
+                <a href="#"> Link 2</a>
+                <a href="#" > Link 2 Link 2 </a>
+                <a href="#"> Link 2 Link 2</a>
+                <a href="#"> Link 2</a>         
+            </div>
+            <div class="footer-inner-1st-column">
+                <h3>Section 3</h3>
+                <a href="#" >Link 2 Link 2 </a>
+                <a href="#"> Link 2 Link 2 </a>
+                <a href="#"> Link 2 </a>
+                <a href="#"> Link 2 </a>
+            </div>
+        </div>
+
+        <div class="footer-inner-break-line">
+
+        </div>
+        <div class="footer-inner-2nd">
+            <div class="footer-inner-2nd-1st">
+                Ⓒ 500plus blah blah blah blah.
+            </div>
+            <div class="footer-inner-2nd-2nd">
+                Lorem ipsum dolor sit amet
             </div>
         </div>
     </footer>
