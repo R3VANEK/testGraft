9c30c8c2dcf5fddcbf83d50fea01451276138323
Arcywojak <maciej778@op.pl>
2020-04-12 14:11:32 +0200

    change text in footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,51 +1,51 @@
 import React from 'react'
 import './footer.css'
 import fb from '../../images/facebook.svg';
-
+ 
 function Footer() {
     return (
         <footer>
         <div class="footer-inner-1st">
             <div class="footer-inner-1st-column">
                 <h3>Section 1</h3>
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 2</a>
+                <a href="/">Home</a>
+                <a href="/o-nas">O Nas</a>
+                <a href="/logowanie">Zaloguj się</a>
                 <div class="footer-icons">
                     <img src={fb} alt="fb" />
                     <img src={fb} alt="fb" />
                     <img src={fb} alt="fb" />
-                </div>               
+                </div>              
             </div>
             <div class="footer-inner-1st-column">
-                <h3>Section 2</h3>
-                <a href="#"> Link 2</a>
-                <a href="#" > Link 2 Link 2 </a>
-                <a href="#"> Link 2 Link 2</a>
-                <a href="#"> Link 2</a>         
+                <h3>O nas</h3>
+                <a href="/kontakt"> Kontakt</a>
+                <a href="/o-nas" > O Nas</a>
+                <a href="#"> Dołącz do Nas</a>
+                <a href="#"> Nasza Misja</a>        
             </div>
             <div class="footer-inner-1st-column">
-                <h3>Section 3</h3>
-                <a href="#" >Link 2 Link 2 </a>
-                <a href="#"> Link 2 Link 2 </a>
-                <a href="#"> Link 2 </a>
-                <a href="#"> Link 2 </a>
+                <h3>Informacje</h3>
+                <a href="/regulamin" >Regulamin </a>
+                <a href="#"> Polityka Prywatności  </a>
+                <a href="/bezpieczenstwo"> Bezpieczeństwo  </a>
+                <a href="#">Wytyczne dla społeczności</a>
             </div>
         </div>
-
+ 
         <div class="footer-inner-break-line">
-
+ 
         </div>
         <div class="footer-inner-2nd">
             <div class="footer-inner-2nd-1st">
-                Ⓒ 500plus blah blah blah blah.
+                Ⓒ 2020 <a href="/">coza500plus.pl</a>
             </div>
             <div class="footer-inner-2nd-2nd">
-                Lorem ipsum dolor sit amet
+                TWOJE Zakupy, po prostu...
             </div>
         </div>
     </footer>
     );
   }
-  
+ 
   export default Footer;
